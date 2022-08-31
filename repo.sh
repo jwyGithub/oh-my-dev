fbb9933bbda3a44874e016c680046ea011b72276
jiangwy <jiangwy@staryea.com>
2022-08-31 16:49:26 +0800

    feature : 区分linux版本与Stream版本

--- repo.sh
+++ repo.sh
@@ -1,25 +1,43 @@
+# Determine whether it is the Linux version or the stream version
 
-# backup CentOS-Stream-AppStream.repo
+centos_linux="CentOS Linux"
+centos_stream="CentOS Stream"
+redhat_release=$(cat /etc/redhat-release | grep "${centos_linux}")
+if [[ "$redhat_release" != "" ]]
+then
+        release="Linux"
+else
+        release="Stream"
+fi
 
-cp /etc/yum.repos.d/CentOS-Stream-AppStream.repo /etc/yum.repos.d/CentOS-Stream-AppStream.repo.backup
+base_os=/etc/yum.repos.d/CentOS-$release-BaseOS.repo
+app_stream=/etc/yum.repos.d/CentOS-$release-AppStream.repo
+
+echo BaseOS file is : $base_os
+echo AppStream file is : $app_stream
 
 
-# backup CentOS-Stream-BaseOS.repo
+# backup base_os
 
-cp /etc/yum.repos.d/CentOS-Stream-BaseOS.repo /etc/yum.repos.d/CentOS-Stream-BaseOS.repo.backup
+cp /etc/yum.repos.d/CentOS-$release-BaseOS.repo /etc/yum.repos.d/CentOS-$release-BaseOS.repo.backup
 
 
-# remove CentOS-Stream-BaseOS.repo
-rm -rf CentOS-Stream-BaseOS.repo
+# backup app_stream
 
-# append CentOS-Stream-BaseOS.repo for aliyun
-echo "$(curl https://raw.githubusercontent.com/jwyGithub/CentOS/main/repo/CentOS-Stream-BaseOS.repo)" >> /etc/yum.repos.d/CentOS-Stream-BaseOS.repo
+cp /etc/yum.repos.d/CentOS-$release-AppStream.repo /etc/yum.repos.d/CentOS-$release-AppStream.repo.backup
 
-# remove CentOS-Stream-BaseOS.repo
-rm -rf CentOS-Stream-AppStream.repo
 
-# append CentOS-Stream-AppStream.repo for aliyun
-echo "$(curl https://raw.githubusercontent.com/jwyGithub/CentOS/main/repo/CentOS-Stream-AppStream.repo)" >> /etc/yum.repos.d/CentOS-Stream-AppStream.repo
+# remove base_os
+rm -rf /etc/yum.repos.d/CentOS-$release-BaseOS.repo
+
+# append base_os for aliyun
+echo "$(curl https://raw.githubusercontent.com/jwyGithub/CentOS/main/repo/CentOS-Stream-BaseOS.repo)" >> /etc/yum.repos.d/CentOS-$release-BaseOS.repo
+
+# remove app_stream
+rm -rf /etc/yum.repos.d/CentOS-$release-AppStream.repo
+
+# append app_stream for aliyun
+echo "$(curl https://raw.githubusercontent.com/jwyGithub/CentOS/main/repo/CentOS-Stream-AppStream.repo)" >> /etc/yum.repos.d/CentOS-$release-AppStream.repo
 
 
 # cache
